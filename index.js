2bb59853f0346026beb3faccb0f50b832f480b0f
baram55 <asd7973@gmail.com>
2024-04-08 00:29:28 +0900

    [Script] Cors 패키지 제거

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { cors } from "cors";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
 const httpServer = createServer(app);
 const port = 4000;
-const io = new Server(httpServer);
-
-io.on("connection", (socket) => {
-  console.log("연결됨");
-  socket.emit("소켓과 연결되었습니다.");
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
-app.listen(port, () => {
+app.get("/", (req, res) => {
+  res.send("connected");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+});
+
+io.on("disconnection", (socket) => {
+  console.log("A user connected");
+});
+
+httpServer.listen(port, () => {
   console.log(`port(${port})으로 실행 중`);
 });
