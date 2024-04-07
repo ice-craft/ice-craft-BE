4a85c2e0f3684f554aa09400da0743b511e6a685
baram55 <asd7973@gmail.com>
2024-04-07 23:30:30 +0900

    [Script] Cors 패키지 설치

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { cors } from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 const httpServer = createServer(app);
 const port = 4000;
 const io = new Server(httpServer);
