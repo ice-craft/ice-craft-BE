0715e87751cecf2024d5ba29c4e092518ffc308b
baram55 <asd7973@gmail.com>
2024-04-30 05:52:44 +0900

    [Fix] r1DecideDoctorToSavePlayer 변경 기능 수정

--- index.js
+++ index.js
@@ -728,7 +728,7 @@ mafiaIo.on("connection", (socket) => {
 
     try {
       const { total_user_count } = await getUserCountInRoom(roomId);
-      await setStatus(userId, { r1DecideDoctorToSavePlayer: true });
+      await setStatus(userId, roomId, "r1DecideDoctorToSavePlayer", true);
       isDone = await getStatus(
         roomId,
         "r1DecideDoctorToSavePlayer",
