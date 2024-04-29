d98d1d8055bbe7447750b933dc3d9cfc35df6402
baram55 <asd7973@gmail.com>
2024-04-30 05:51:24 +0900

    [Fix] r1TurnMafiaUserCameraOn 변경 기능 수정

--- index.js
+++ index.js
@@ -649,7 +649,7 @@ mafiaIo.on("connection", (socket) => {
 
     try {
       const { total_user_count } = await getUserCountInRoom(roomId);
-      await setStatus(userId, { r1TurnMafiaUserCameraOn: true });
+      await setStatus(userId, roomId, "r1TurnMafiaUserCameraOn", true);
       isDone = await getStatus(
         roomId,
         "r1TurnMafiaUserCameraOn",
