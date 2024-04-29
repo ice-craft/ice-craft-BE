7367937674fadfacb7799afa1dc4690052b88cdc
baram55 <asd7973@gmail.com>
2024-04-30 05:48:54 +0900

    [Fix] r1LastTalk 변경 기능 수정

--- index.js
+++ index.js
@@ -499,7 +499,7 @@ mafiaIo.on("connection", (socket) => {
 
     try {
       const { total_user_count } = await getUserCountInRoom(roomId);
-      await setStatus(userId, { r1LastTalk: true });
+      await setStatus(userId, roomId, "r1LastTalk", true);
       isDone = await getStatus(roomId, "r1LastTalk", total_user_count);
     } catch (error) {
       console.log("[r1LastTalkError]");
