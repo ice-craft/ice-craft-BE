3d7fc994aa84669348b978cabe8678d930b5c788
baram55 <asd7973@gmail.com>
2024-05-01 00:46:37 +0900

    [Fix]         r1DecidePoliceToDoubtPlayer  기능 수정

--- index.js
+++ index.js
@@ -960,7 +960,17 @@ mafiaIo.on("connection", (socket) => {
 
     try {
       const { total_user_count } = await getUserCountInRoom(roomId);
-      await setStatus(userId, roomId, "r1DecidePoliceToDoubtPlayer", true);
+      const isValid = await setStatus(
+        userId,
+        roomId,
+        "r1DecidePoliceToDoubtPlayer",
+        true
+      );
+
+      if (!isValid) {
+        throw new Error();
+      }
+
       isDone = await getStatus(
         roomId,
         "r1DecidePoliceToDoubtPlayer",
