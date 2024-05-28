b88768ef142a53868819e89a83b0942346d1699f
baram55 <asd7973@gmail.com>
2024-05-28 20:50:24 +0900

    [Fix] r2-2 라운드 테스트 코드 수정

--- api/supabase/client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
