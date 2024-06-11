7de2a6dfc2d5bd3795b9967be6b72a65a8d64fd4
baram55 <asd7973@gmail.com>
2024-06-12 02:58:51 +0900

    [Fix] dbTest에서 사용하지 않는 메서드 import 제거

--- dbTest.js
+++ dbTest.js
@@ -1,21 +1,4 @@
-import {
-  getCurrentUserDisplay,
-  getPlayersInRoom,
-  getRoleMaxCount,
-  getRound,
-  resetPlayerStatus,
-  resetRoundR0,
-  resetRoundR1,
-  resetRoundR2,
-  setStatus,
-  updateRound,
-} from "./api/supabase/gamePlayAPI.js";
-import {
-  decideChief,
-  getUserIdInRoom,
-  isChiefExisted,
-  setChief,
-} from "./api/supabase/roomAPI.js";
+import { decideChief } from "./api/supabase/roomAPI.js";
 
 try {
   const data = await decideChief("0ed9a099-f1b4-46eb-a187-2da752eed29c");
