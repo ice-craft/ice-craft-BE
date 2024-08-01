6e5a2762e916858e8023568219344af03a619f60
baram55 <asd7973@gmail.com>
2024-08-02 08:23:40 +0900

    [Fix] pm2의 실행 앱들을 분리

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: "ice-craft",
+      name: "ice-craft-cluster",
       script: "src/app.ts",
       instances: 0,
       exec_mode: "cluster",
       autorestart: true,
+      max_memory_restart: "1G",
 
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "ice-craft-fork",
+      script: "src/app.ts",
+      exec_mode: "fork",
+      autorestart: true,
       max_memory_restart: "1G",
 
       env: {
