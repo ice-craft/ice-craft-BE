9102fd9628629116bda48ca7680be21aa58a45b0
baram55 <asd7973@gmail.com>
2024-08-01 17:06:07 +0900

    [Fix] pm2 설정 파일 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,10 @@ module.exports = {
     {
       name: "ice-craft",
       script: "src/app.ts",
-      instances: 1,
+      instances: 0,
       exec_mode: "cluster",
       autorestart: true,
-      watch: true,
       max_memory_restart: "1G",
-
-      watch: ["src"],
-      ignore_watch: ["node_modules"],
 
       env: {
         NODE_ENV: "production",
